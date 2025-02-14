a4e3c7ce41cc679d258b5589b098a1e51c1c4ba3
Ngwayi Joel Tanto <jdstdio@gmail.com>
2025-02-14 06:21:49 +0100

    Updates with code

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+    const SimpleStorage = await hre.ethers.getContractFactory("SimpleStorage");
+    const simpleStorage = await SimpleStorage.deploy();
+    await simpleStorage.deployed();
+    console.log(`Contract deployed to: ${simpleStorage.address}`);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
